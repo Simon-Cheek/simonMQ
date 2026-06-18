6fb85a95af5b9040026db7487fd456a83e2f1183
Simon-Cheek <simon.cheek@yahoo.com>
2026-06-17 21:03:05 -0400

    Implement methods on broker

--- simple-mq/broker.go
+++ simple-mq/broker.go
@@ -1,11 +1,16 @@
 package main
 
+import "sync"
+import "github.com/google/uuid"
+
 type Broker struct {
 	queues map[string]*Queue
+	mu     sync.Mutex
 }
 
 type Queue struct {
-	name     string
+	name string
+	// TODO: Refactor to use more efficient data structure
 	messages []*QueueMsg
 }
 
@@ -17,8 +22,39 @@ type QueueMsg struct {
 func (b *Broker) enqueue(name string, payload string) *QueueMsg {
 
 	msg := &QueueMsg{
-		MsgId:   "",
+		MsgId:   name + "/" + uuid.New().String(),
 		Payload: payload,
 	}
+
+	// TODO: Lock by Queue instead of globally
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	val, exists := b.queues[name]
+	if exists && len(val.messages) > 0 {
+		val.messages = append(val.messages, msg)
+	} else {
+		b.queues[name] = &Queue{
+			name:     name,
+			messages: []*QueueMsg{msg},
+		}
+	}
+
 	return msg
+}
+
+func (b *Broker) dequeue(name string) *QueueMsg {
+
+	// TODO: Lock by Queue instead of globally
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	val, exists := b.queues[name]
+	if exists {
+		msg := val.messages[0]
+		val.messages = val.messages[1:]
+		return msg
+	} else {
+		return nil
+	}
 }
