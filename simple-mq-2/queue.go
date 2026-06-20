268e61f0f3a40c33571a4e3d98da1630b37b053b
Simon-Cheek <simon.cheek@yahoo.com>
2026-06-20 10:49:16 -0400

    Fix naive queue implementation'

--- simple-mq-2/queue.go
+++ simple-mq-2/queue.go
@@ -1,12 +1,15 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 type Queue struct {
 	name  string
-	count int // Total Unread Msgs
-	head  int // Location of last unread msg
-	buf   []*QueueMsg
+	count int         // Total Unread Msgs
+	head  int         // Location of last unread msg
+	buf   []*QueueMsg // Ring buffer
 	mu    sync.Mutex
 }
 
@@ -19,38 +22,57 @@ func (q *Queue) add(msg *QueueMsg) {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
-	if q.head+q.count >= len(q.buf) {
+	ind := (q.head + q.count) % len(q.buf)
+	q.buf[ind] = msg
+	q.count++
+
+	if q.count*2 > len(q.buf) {
 		q.grow()
 	}
-	q.buf[q.head+q.count] = msg
-	q.count++
 }
 
 func (q *Queue) pop() *QueueMsg {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
+	if q.count == 0 {
+		return nil
+	}
+
 	msg := q.buf[q.head]
-	q.head++
+	q.head = (q.head + 1) % len(q.buf)
+	q.count--
 
 	if q.count*3 < len(q.buf) && len(q.buf) > 100 {
 		q.compact()
 	}
 
 	return msg
-
 }
 
 func (q *Queue) grow() {
 	newBuf := make([]*QueueMsg, len(q.buf)*2)
-	copy(newBuf, q.buf[:q.head])
-	q.buf = newBuf
-	q.head = 0
+	q.copyOver(newBuf)
 }
 
 func (q *Queue) compact() {
-	newBuf := make([]*QueueMsg, q.count*2)
-	copy(newBuf, q.buf[:q.head])
+	newBuf := make([]*QueueMsg, len(q.buf)/2)
+	q.copyOver(newBuf)
+}
+
+func (q *Queue) copyOver(newBuf []*QueueMsg) {
+	if q.count > len(newBuf) {
+		panic(fmt.Sprintf("copyOver: count %d exceeds newBuf capacity %d", q.count, len(newBuf)))
+	}
+
+	copy(newBuf, q.buf[q.head:])
+	nItemsCopied := len(q.buf[q.head:])
+	remainingItemsToCopy := q.count - nItemsCopied
+
+	// Wrap Around Case
+	if remainingItemsToCopy > 0 {
+		copy(newBuf[nItemsCopied:], q.buf[:remainingItemsToCopy])
+	}
 	q.buf = newBuf
 	q.head = 0
 }
