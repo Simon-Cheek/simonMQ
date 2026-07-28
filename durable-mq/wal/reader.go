4bfef27827c37ec2c3d70611c725ee524ee3404c
Simon-Cheek <simon.cheek@yahoo.com>
2026-07-27 21:01:22 -0400

    Segment mgr

--- durable-mq/wal/reader.go
+++ durable-mq/wal/reader.go
@@ -6,40 +6,82 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 type Reader struct {
-	file *os.File
-}
-
-func OpenReader(filename string) (*Reader, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Reader{file}, nil
-}
-
-func (r *Reader) Close() error {
-	return r.file.Close()
+	dir      string
+	segments []string
+	idx      int
+	file     *os.File
 }
 
 // Any Log bigger than this likely means some sort of corrupt data issue
 const maxLogLength = 16 * 1024 * 1024
 
+func OpenReader(dir string) (*Reader, error) {
+	segs, err := listSegments(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	r := &Reader{dir: dir, segments: segs}
+	if len(segs) > 0 {
+		if err := r.openSegment(0); err != nil {
+			return nil, err
+		}
+	}
+	return r, nil
+}
+
+func (r *Reader) openSegment(idx int) error {
+	f, err := os.Open(filepath.Join(r.dir, r.segments[idx]))
+	if err != nil {
+		return err
+	}
+	r.file = f
+	r.idx = idx
+	return nil
+}
+
+func (r *Reader) isLastSegment() bool {
+	return r.idx == len(r.segments)-1
+}
+
 // TODO: Add logic to handle hardware corruption, checksum corruption, etc
+// TODO: Add ReadAll() version that streams / iterates
 func (r *Reader) ReadAll() ([]*record.Record, error) {
 	var records []*record.Record
 
+	if len(r.segments) == 0 {
+		return records, nil
+	}
+	if err := r.openSegment(0); err != nil {
+		return nil, err
+	}
+
 	for {
-		header := make([]byte, 12) // Enough to hold LSN and length indicator
+		header := make([]byte, 12)
 		_, err := io.ReadFull(r.file, header)
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			break
+		if err == io.EOF {
+			if r.isLastSegment() {
+				return records, nil
+			}
+			if err := r.openSegment(r.idx + 1); err != nil {
+				return nil, err
+			}
+			continue
+		}
+		if err == io.ErrUnexpectedEOF {
+			if r.isLastSegment() {
+				return records, nil
+			}
+			return records, fmt.Errorf("unexpected partial record in non-final segment %s", r.segments[r.idx])
 		}
 		if err != nil {
 			return records, err
 		}
+
 		remainingLength := binary.LittleEndian.Uint32(header[8:12])
 		if remainingLength > maxLogLength {
 			return records, fmt.Errorf("wal log over max header length: %d", remainingLength)
@@ -48,15 +90,19 @@ func (r *Reader) ReadAll() ([]*record.Record, error) {
 		remaining := make([]byte, remainingLength)
 		_, err = io.ReadFull(r.file, remaining)
 		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			break
+			if r.isLastSegment() {
+				return records, nil
+			}
+			return records, fmt.Errorf("unexpected partial record in non-final segment %s", r.segments[r.idx])
 		}
 		if err != nil {
 			return records, err
 		}
+
 		recordBytes := append(header, remaining...)
 		rec, err := record.Decode(recordBytes)
 		if err != nil {
-			break // CRC Mismatch
+			break
 		}
 		records = append(records, rec)
 	}
--- durable-mq/wal/writer.go
+++ durable-mq/wal/writer.go
@@ -3,24 +3,40 @@ package wal
 import (
 	"durable-mq/record"
 	"os"
+	"path/filepath"
 )
 
 // Assumes SINGLE THREADED writes, this is NOT threadsafe
 type Writer struct {
-	file    *os.File
-	nextLSN uint64
+	dir         string
+	file        *os.File
+	nextLSN     uint64
+	currentSize int64
+	maxSegSize  int64
 }
 
-func OpenWriter(filename string) (*Writer, error) {
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
+func OpenWriter(dir string, mexSegSize int64) (*Writer, error) {
+	w := &Writer{dir: dir, maxSegSize: mexSegSize, nextLSN: 1}
+	if err := w.rollSegment(); err != nil {
 		return nil, err
 	}
-	return &Writer{f, 1}, nil
+	return w, nil
 }
 
-func (w *Writer) Close() error {
-	return w.file.Close()
+func (w *Writer) rollSegment() error {
+	if w.file != nil {
+		if err := w.file.Close(); err != nil {
+			return err
+		}
+	}
+	name := segmentFileName(w.nextLSN)
+	f, err := os.OpenFile(filepath.Join(w.dir, name), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	w.file = f
+	w.currentSize = 0
+	return nil
 }
 
 func (w *Writer) Append(rec *record.Record) (lsn uint64, err error) {
@@ -29,12 +45,21 @@ func (w *Writer) Append(rec *record.Record) (lsn uint64, err error) {
 	if err != nil {
 		return 0, err
 	}
+
+	if (int64(len(buf)) + w.currentSize) > w.maxSegSize {
+		if err := w.rollSegment(); err != nil {
+			return 0, err
+		}
+	}
+
 	if _, err = w.file.Write(buf); err != nil {
 		return 0, err
 	}
 	if err = w.file.Sync(); err != nil {
 		return 0, err
 	}
+
+	w.currentSize += int64(len(buf))
 	w.nextLSN++
 	return rec.LSN, nil
 }
